630248707f33110a3bfa01727184fa70495129f7
Saumya <imsaumya48@gmail.com>
2026-01-18 17:17:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+function generatePassword() {
+  const length = document.getElementById("length").value;
+  const uppercase = document.getElementById("uppercase").checked;
+  const lowercase = document.getElementById("lowercase").checked;
+  const numbers = document.getElementById("numbers").checked;
+  const symbols = document.getElementById("symbols").checked;
+
+  let chars = "";
+  if (uppercase) chars += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  if (lowercase) chars += "abcdefghijklmnopqrstuvwxyz";
+  if (numbers) chars += "0123456789";
+  if (symbols) chars += "!@#$%^&*()_+";
+
+  if (chars === "") {
+    alert("Select at least one option!");
+    return;
+  }
+
+  let password = "";
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * chars.length);
+    password += chars[randomIndex];
+  }
+
+  document.getElementById("password").value = password;
+}
+
+function copyPassword() {
+  const password = document.getElementById("password");
+  password.select();
+  navigator.clipboard.writeText(password.value);
+  document.getElementById("copyMsg").innerText = "Copied ✔";
+}
